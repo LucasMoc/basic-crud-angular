5151d6d7d44e03d20c57ca43ba55ee0f693bd121
lucas.mocelin <lucas.mocelin@maxiconsystems.com.br>
2022-10-18 18:17:32 -0300

    core project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { PageCrudComponent } from './page/page-crud/page-crud.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageCrudComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
